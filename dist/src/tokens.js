9858c442b020adf760c6fd9780c52eed3060ecdb
alxndrsn <alxndrsn>
2024-04-16 09:45:20 +0000

    4

--- dist/src/tokens.js
+++ dist/src/tokens.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UnnestToken = exports.SqlToken = exports.ListToken = exports.JsonToken = exports.IdentifierToken = exports.ComparisonPredicateToken = exports.BinaryToken = exports.ArrayToken = void 0;
-exports.ArrayToken = Symbol('SLONIK_TOKEN_ARRAY');
-exports.BinaryToken = Symbol('SLONIK_TOKEN_BINARY');
-exports.ComparisonPredicateToken = Symbol('SLONIK_TOKEN_COMPARISON_PREDICATE');
-exports.IdentifierToken = Symbol('SLONIK_TOKEN_IDENTIFIER');
-exports.JsonToken = Symbol('SLONIK_TOKEN_JSON');
-exports.ListToken = Symbol('SLONIK_TOKEN_LIST');
-exports.SqlToken = Symbol('SLONIK_TOKEN_SQL');
-exports.UnnestToken = Symbol('SLONIK_TOKEN_UNNEST');
+exports.ArrayToken = Symbol.for('SLONIK_TOKEN_ARRAY');
+exports.BinaryToken = Symbol.for('SLONIK_TOKEN_BINARY');
+exports.ComparisonPredicateToken = Symbol.for('SLONIK_TOKEN_COMPARISON_PREDICATE');
+exports.IdentifierToken = Symbol.for('SLONIK_TOKEN_IDENTIFIER');
+exports.JsonToken = Symbol.for('SLONIK_TOKEN_JSON');
+exports.ListToken = Symbol.for('SLONIK_TOKEN_LIST');
+exports.SqlToken = Symbol.for('SLONIK_TOKEN_SQL');
+exports.UnnestToken = Symbol.for('SLONIK_TOKEN_UNNEST');
 //# sourceMappingURL=tokens.js.map
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,8 +1,8 @@
-export const ArrayToken = Symbol('SLONIK_TOKEN_ARRAY');
-export const BinaryToken = Symbol('SLONIK_TOKEN_BINARY');
-export const ComparisonPredicateToken = Symbol('SLONIK_TOKEN_COMPARISON_PREDICATE');
-export const IdentifierToken = Symbol('SLONIK_TOKEN_IDENTIFIER');
-export const JsonToken = Symbol('SLONIK_TOKEN_JSON');
-export const ListToken = Symbol('SLONIK_TOKEN_LIST');
-export const SqlToken = Symbol('SLONIK_TOKEN_SQL');
-export const UnnestToken = Symbol('SLONIK_TOKEN_UNNEST');
+export const ArrayToken = Symbol.for('SLONIK_TOKEN_ARRAY');
+export const BinaryToken = Symbol.for('SLONIK_TOKEN_BINARY');
+export const ComparisonPredicateToken = Symbol.for('SLONIK_TOKEN_COMPARISON_PREDICATE');
+export const IdentifierToken = Symbol.for('SLONIK_TOKEN_IDENTIFIER');
+export const JsonToken = Symbol.for('SLONIK_TOKEN_JSON');
+export const ListToken = Symbol.for('SLONIK_TOKEN_LIST');
+export const SqlToken = Symbol.for('SLONIK_TOKEN_SQL');
+export const UnnestToken = Symbol.for('SLONIK_TOKEN_UNNEST');
